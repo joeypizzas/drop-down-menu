f9421007d17635e0107aec6774372d3287b95205
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-09-27 09:43:32 -0400

    Finished styling dropdown menu on hover + comments

--- src/dropdown.js
+++ src/dropdown.js
@@ -3,30 +3,49 @@
 export function initDropdownListeners() {
   const root = document.documentElement;
   const dropdownItems = document.querySelectorAll(".dropdown-item");
-  const dropdownParents = document.querySelectorAll(".dropdown-parent");
+  const dropdownChildren = document.querySelectorAll(".dropdown-child");
+  const dropdownLists = document.querySelectorAll(".dropdown-list");
+
   dropdownItems.forEach((item) => {
+    // Hover effects for all items across menus
     item.addEventListener("mouseover", () => {
       item.style.backgroundColor =
         getComputedStyle(root).getPropertyValue("--hover");
+      item.style.color =
+        getComputedStyle(root).getPropertyValue("--text-hover");
     });
     item.addEventListener("mouseout", () => {
       item.style.backgroundColor =
         getComputedStyle(root).getPropertyValue("--normal");
+      item.style.color = getComputedStyle(root).getPropertyValue("--text");
     });
   });
-  dropdownParents.forEach((parent) => {
-    const dropdownList = parent.closest(".dropdown-list");
-    const dropdownChildren = dropdownList.querySelectorAll(".dropdown-child");
-    parent.addEventListener("mouseover", () => {
-      dropdownChildren.forEach((child) => {
-        child.classList.add("shown");
-      });
+  dropdownChildren.forEach((child) => {
+    // Click effects for children across all menus
+    child.addEventListener("mousedown", () => {
+      child.style.backgroundColor =
+        getComputedStyle(root).getPropertyValue("--click");
+      child.style.color =
+        getComputedStyle(root).getPropertyValue("--text-click");
     });
-    dropdownList.addEventListener("mouseout", () => {
-      // start here
-      dropdownChildren.forEach((child) => {
-        child.classList.remove("shown");
-      });
+    child.addEventListener("mouseup", () => {
+      child.style.backgroundColor =
+        getComputedStyle(root).getPropertyValue("--hover");
+      child.style.color =
+        getComputedStyle(root).getPropertyValue("--text-hover");
+    });
+  });
+  dropdownLists.forEach((list) => {
+    const children = list.querySelectorAll(".dropdown-child");
+    const parent = list.querySelector(".dropdown-parent");
+
+    parent.addEventListener("mouseover", () => {
+      // Show/hide children on specific menu when hover on parent
+      children.forEach((child) => child.classList.add("shown"));
+    });
+
+    list.addEventListener("mouseleave", () => {
+      children.forEach((child) => child.classList.remove("shown"));
     });
   });
 }
